6ed05e45e085ed623b0345bf89c7ff397dd82396
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-01 09:09:35 -0600

    working commit - Created using LeetHub

---
+++ shortest-word-distance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(word2)) {
+                        minDistance = Math.min(minDistance, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDistance;
+    }
+}
