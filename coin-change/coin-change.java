8dc7abdea91221b91c770c22d81898b60df2c718
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-07 10:08:42 -0600

    working commit - Created using LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,12 +1,14 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1]; 
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
+        for (int i = 0; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
+                // check if the current coin is <= amount which is (i)
                 if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);                    
+                    // find the minimum
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1); // i cents - current coin we are at
                 }
             }
         }
