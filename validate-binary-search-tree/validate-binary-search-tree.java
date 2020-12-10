ccf64b255bfa66c9052ec6d4d3c8323ebd740caf
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-10 15:34:13 -0600

    working commit - Created using LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+    Stack<TreeNode> stack = new Stack();
+    double inorder = - Double.MAX_VALUE;
+​
+    while (!stack.isEmpty() || root != null) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      // If next element in inorder traversal
+      // is smaller than the previous one
+      // that's not BST.
+      if (root.val <= inorder) return false;
+      inorder = root.val;
+      root = root.right;
+    }
+    return true;
+    }
+}
