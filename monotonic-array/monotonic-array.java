5ba883e01fb80e78614c6837ce4a21c3245a0b50
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-11 14:03:06 -0600

    Time: 1 ms (99.96%), Space: 47.7 MB (36.24%) - LeetHub

---
+++ monotonic-array/monotonic-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < A.length - 1; i++) {
+            // find out if the A is increasing
+            if (A[i] < A[i + 1]) decreasing = false;
+            if (A[i] > A[i + 1]) increasing = false;
+        }
+        return increasing || decreasing;
+    }
+}
