b2060d0e72f003c7ef9d1ab59b2db935eb6eafc0
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-23 13:16:11 -0600

    Time: 0 ms (100.00%), Space: 35.5 MB (89.77%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,24 +1,34 @@
 class Solution {
-    /*
-        [[1,1,1,1,1,1],
-         [1,2,3,4,5,6],
-         [1,3,6,10,15,21],
-         [1,4,10,20,35,56],
-         [1,5,15,35,70,126]]
-    */
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        /*
+            1. The robot can only move either DOWN or RIGHT at each point in time
+            2. The answer is found at M-1, N-1
+            3. How many unique paths?
+            
+            We can solve this problem using DP approach 
+            Iterate over each row and col, and compute each step we can make 
+                - dp[i-1][j]
+                - dp[i][j-1]
+            return dp[n-1][m-1] because that's where out answer will be stored 
+            
+        */
+        int row = m;
+        int col = n;
         
+        int[][] dp = new int[row][col];
+        
+        // we need to kick start our DP
         for (int[] arr: dp) {
             Arrays.fill(arr, 1);
         }
         
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        // start at position (since we kickstarted above) 
+        // and computer the two moves we can make in dp[i][j]
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        System.out.print(dp);
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
