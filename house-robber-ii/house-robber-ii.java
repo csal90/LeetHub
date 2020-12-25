b79f0d3868162e3ad7e3e7db911c0c9567341c8d
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-24 21:30:20 -0600

    Time: 0 ms (100.00%), Space: 36.1 MB (96.03%) - LeetHub

--- house-robber-ii/house-robber-ii.java
+++ house-robber-ii/house-robber-ii.java
@@ -1,31 +1,25 @@
 class Solution {
     public static int rob(int[] nums) {
         int N = nums.length;
-        
-        if (N == 0) {
-            return 0;
-        }
         if (N == 1) {
             return nums[0];
         }
         if (N == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        int [] a = Arrays.copyOfRange(nums, 1, N);
-        int [] b = Arrays.copyOfRange(nums, 0, N - 1);
-        return Math.max(robMax(a), robMax(b));
+        int [] p1 = Arrays.copyOfRange(nums, 1, N);
+        int [] p2 = Arrays.copyOfRange(nums, 0, N - 1);
+        return Math.max(robHelper(p1), robHelper(p2));
     }
     
-    public static int robMax(int[] nums) {
+    public static int robHelper(int[] nums) {
         int N = nums.length;
         int[] dp = new int[N];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        
         for (int i = 2; i < N; i++) {
             dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
         return dp[N - 1];
     }
-    
 }
