b7cd74a25a0e6f11971d2ea926540afafbdf716c
Christian Salas <35342933+csal90@users.noreply.github.com>
2020-12-31 12:09:05 -0600

    Time: 14 ms (22.59%), Space: 39.7 MB (26.83%) - LeetHub

---
+++ valid-sudoku/valid-sudoku.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char curr = board[i][j];
+                if (curr != '.') {
+                    
+                    if (!seen.add(curr + " found in row " + i) ||
+                        !seen.add(curr + " found in col " + j) ||
+                        !seen.add(curr + " found in sub box " + i/3 + " - " + j/3)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+        
+    }
+}
