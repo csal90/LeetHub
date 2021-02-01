94a53e67595aea795cc8427080949415a90cf724
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-01 14:06:37 -0600

    Time: 3 ms (21.10%), Space: 38.9 MB (96.21%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,36 +1,19 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        // Output array and nums in a List 
-        List<List<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> numsList = new ArrayList<>();
-        
-        // Convert nums array to a List
-        for (int i = 0; i < nums.length; i++) {
-            numsList.add(nums[i]);
-        }
-        
-        int n = nums.length;
-        backtrack(n, numsList, res, 0);
-        return res;
-    }
-    
-    public void backtrack(int n, 
-                          ArrayList<Integer> nums, 
-                          List<List<Integer>> res, 
-                          int first) {
-        // if all integers have been used 
-        if (first == n) {
-            res.add(new ArrayList<Integer>(nums));
-        }
-        // start a permutation
-        for (int i = first; i < n; i++) {
-            // place i-th integer first
-            Collections.swap(nums, first, i);
-            // use next integers to complete permutation
-            backtrack(n, nums, res, first + 1);
-            // backtrack
-            Collections.swap(nums, first, i);
-        }
-    }
-​
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generate(nums, result, new ArrayList());
+        return result;
+    }
+    private void generate(int[] nums, List<List<Integer>> result, List<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList(list));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) 
+                continue;
+            list.add(nums[i]);
+            generate(nums, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
 }
