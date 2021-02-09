5107a928f7e31415651add79069892ed98703dc6
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-09 13:56:47 -0600

    Time: 1 ms (100.00%), Space: 49.8 MB (68.81%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,29 +1,39 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int N = nums.length;
-        
-        int[] L = new int[N];
-        int[] R = new int[N];
-        int[] res = new int[N];
-        
-        L[0] = 1;
-        R[N-1] = 1;
-        
-        // Add product of everything to the left of (i)
-        for (int i = 1; i < N; i++) {
-            L[i] = nums[i-1] * L[i-1];
-        }
-        
-        // Add product of everything to the right of (i)
-        for (int i = N-2; i >= 0; i--) {
-            R[i] = nums[i+1] * R[i+1];
-        }
-        
-        // Move L and R to the res array
-        for (int i = 0; i < N; i++) {
-            res[i] = L[i] * R[i];
-        }
-        
-        return res;
-    }
+    public int[] productExceptSelf(int[] nums) {
+        int N = nums.length; 
+        
+        // int[] L = new int[N];
+        // int[] R = new int[N];
+        int[] answer = new int[N];
+        
+//         L[0] = 1;
+//         R[N - 1] = 1;
+        
+//         for (int i = 1; i < N; i++) {
+//             L[i] = L[i - 1] * nums[i - 1];
+//         }
+        
+//         for (int i = N - 2; i >= 0; i--) {
+//             R[i] = R[i + 1] * nums[i + 1];
+//         }
+        // for (int i = 0; i < N; i++) {
+        //     result[i] = L[i] * R[i];
+        // }   
+        
+        //return result;
+        
+        answer[0] = 1;
+        for (int i = 1; i < N; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        
+        int R = 1;
+        for (int i = N - 1; i >= 0; i--) {
+            answer[i] = answer[i] * R;
+            R *= nums[i];
+        }
+        
+        return answer;
+        
+    }
 }
