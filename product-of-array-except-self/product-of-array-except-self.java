f157f7f66f08b9b73710184fea5de273c8243492
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-10 20:31:19 -0600

    Time: 1 ms (100.00%), Space: 52.1 MB (38.76%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -2,38 +2,27 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int N = nums.length; 
         
-        // int[] L = new int[N];
-        // int[] R = new int[N];
+        int[] L = new int[N];
+        int[] R = new int[N];
         int[] answer = new int[N];
         
-//         L[0] = 1;
-//         R[N - 1] = 1;
-        
-//         for (int i = 1; i < N; i++) {
-//             L[i] = L[i - 1] * nums[i - 1];
-//         }
-        
-//         for (int i = N - 2; i >= 0; i--) {
-//             R[i] = R[i + 1] * nums[i + 1];
-//         }
-        // for (int i = 0; i < N; i++) {
-        //     result[i] = L[i] * R[i];
-        // }   
-        
-        //return result;
-        
-        answer[0] = 1;
+        // product of elements to left 
+        L[0] = 1;
         for (int i = 1; i < N; i++) {
-            answer[i] = nums[i - 1] * answer[i - 1];
+            L[i] = L[i - 1] * nums[i - 1];
         }
         
-        int R = 1;
-        for (int i = N - 1; i >= 0; i--) {
-            answer[i] = answer[i] * R;
-            R *= nums[i];
+        // product of elements to right 
+        R[N - 1] = 1;
+        for (int i = N - 2; i >= 0; i--) {
+            R[i] = R[i + 1] * nums[i + 1];
+        }
+        
+        // combine results into answer array 
+        for (int i = 0; i < N; i++) {
+            answer[i] = L[i] * R[i];
         }
         
         return answer;
-        
     }
 }
