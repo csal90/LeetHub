275e781f430861c53e4f654b47f56ef0d43d31e6
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-15 14:44:28 -0600

    Time: 0 ms (100.00%), Space: 35.9 MB (43.15%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -4,16 +4,22 @@ class Solution {
             return 1;
         }
 
-        int[] dp = new int[n + 1];
+        //int[] dp = new int[n + 1];
         
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        //dp[0] = 1;
+        //dp[1] = 1;
         
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 2] + dp[i - 1];
+        int first = 1;
+        int second = 1;
+        int curr = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            //dp[i] = dp[i - 2] + dp[i - 1];
+            curr = first + second;
+            first = second;
+            second = curr;
         }
         
-        return dp[n];
+        return second;
     }
 }
