6b584685d9352f781bd74d6de6228b3cd23e16b0
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-15 14:34:23 -0600

    Time: 0 ms (100.00%), Space: 35.6 MB (71.43%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int fib(int N) {
-        if (N == 0) return 0;
-        int[] dp = new int[N+1];
-        
-        dp[0] = 0;
-        dp[1] = 1;
-    
-        
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[N];
-    }
+    public int fib(int n) {
+        if (n == 0) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 }
