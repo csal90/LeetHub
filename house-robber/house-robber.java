0e85a2789c4dbec9d568806c8233768ec4195aed
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-15 19:01:26 -0600

    Time: 0 ms (100.00%), Space: 36.4 MB (47.03%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,32 +1,16 @@
 class Solution {
-    public int rob(int[] nums) {
-        int N = nums.length;
-        
-        // nums is empty, return 0
-        if (N == 0) {
-          return 0;  
-        }
-        
-        // nums has 1 element, return it 
-        if (N == 1) {
-            return nums[0];
-        }
-        
-        // nums has it least 2 elements, dp[i] = Max Money up until i
-        int[] dp = new int[N];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        
-        // iterate over the nums starting from 2 
-        // apply competing subproblems 
-        for (int i = 2; i < nums.length; i++) {
-​
-            // first competing subproblem
-            int include = nums[i] + dp[i - 2];
-            int dontInclude = dp[i - 1];
-            dp[i] = Math.max(include, dontInclude);
-        }
-        
-        return dp[N-1];
-    }
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        if (nums.length == 1) return nums[0];
+        
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
 }
