9931d79a4f9d79b6a568568df50333d4c44c6146
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-18 19:45:09 -0600

    Time: 1 ms (100.00%), Space: 52.3 MB (34.62%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,28 +1,40 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int N = nums.length; 
+        /*
+                    [4,5,1,8,2]
+            L=>   [1,4,20,20,160]
+                   ^
+            R=>   [80,16,16,2,1]
+                               ^
+                               
+            Result=> [L] * [R]
+            
+        */
+        int N = nums.length;
+        
+        int[] result = new int[N];
         
         int[] L = new int[N];
-        int[] R = new int[N];
-        int[] answer = new int[N];
-        
-        // product of elements to left 
         L[0] = 1;
+        
+        int[] R = new int[N];
+        R[N - 1] = 1;
+        
+        // compute product of everything to the left of nums[i]
         for (int i = 1; i < N; i++) {
             L[i] = L[i - 1] * nums[i - 1];
         }
         
-        // product of elements to right 
-        R[N - 1] = 1;
+        // compute product of everything to the right of nums[i]
         for (int i = N - 2; i >= 0; i--) {
             R[i] = R[i + 1] * nums[i + 1];
         }
         
-        // combine results into answer array 
+        // store the result to result array
         for (int i = 0; i < N; i++) {
-            answer[i] = L[i] * R[i];
+            result[i] = L[i] * R[i];
         }
         
-        return answer;
+        return result;
     }
 }
