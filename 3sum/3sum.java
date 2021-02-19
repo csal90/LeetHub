c01d3c37983b1b7bf7930a100e099e6c1f52e39f
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-18 20:45:34 -0600

    Time: 20 ms (72.35%), Space: 42.7 MB (83.38%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -2,36 +2,29 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        int N = nums.length;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
-            
+        for (int i = 0; i < N; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) 
+                continue;
             int low = i + 1;
-            int high = nums.length - 1;
-            
+            int high = N - 1;
             while (low < high) {
-                int temp = nums[i] + nums[low] + nums[high]; 
-                // if we have answer
-                if (temp == 0) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    // while we have duplicates update your low pointer
                     while (low < high && nums[low] == nums[low + 1]) 
                         low++;
-                    // while we have duplicates update your high pointer 
-                    while (low < high && nums[high] == nums[high - 1])
+                    while (low < high && nums[high] == nums[high - 1]) 
                         high--;
-                    
-                    // there are no duplicates so we can simply update them 
                     low++;
                     high--;
-                }
-                
-                else if (temp < 0) {
+                } else if (sum < 0) {
                     low++;
                 } else {
                     high--;
                 }
-            }   
+            }
         }
         return result;
     }
