fab7c140de3d5616147c487506f654480003cb8f
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-21 16:16:08 -0600

    Time: 1 ms (93.81%), Space: 38.5 MB (94.11%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,22 +1,47 @@
 class Solution {
     public int maxProduct(int[] nums) {
-
-        if (nums.length == 0) return 0;
-
-        int currMax = nums[0];
-        int currMin = nums[0];
-        int result = currMax;
-        
+        /* 
+            Idea:
+                - Keep track of the min at all times
+                - Keep track of the max at all times
+                - Keep track of overall max 
+                
+                minSoFar = nums[0]
+                maxSoFar = nums[0]
+                overAllMax = nums[0]
+                
+                for (int i = 1; i < n; i++) {
+                    minSoFar = Math.min(nums[i], Math.min(nums[i] * minSoFar))
+                    maxSoFar = Math.max(nums[i], Math.max(nums[i] * maxSoFar))
+                    overAllMax = Math.max(minSoFar, maxSoFar)
+                }
+                
+                return overAllMax
+            
+                [2,3,-2,4]
+                minSoFar = 2, 2, -2
+                MaxSoFar = 6
+                overAll  = 6
+                
+                minSoFar = min {2, min{3 * 2, 3}} => 2
+                maxSoFar = max {2, max{3 * 2, 3}} => 6
+                
+                minSoFar = min {2, min{-2 * 2, -2}} => -2
+                minSoFar = max {2, max{3 * 2, 3}} => 6
+                
+                
+        */
+        int minSoFar = nums[0];
+        int maxSoFar = nums[0];
+        int overAllMax = maxSoFar;
+                
         for (int i = 1; i < nums.length; i++) {
-            
-            int tmpMax = Math.max(nums[i], Math.max(currMin * nums[i], currMax * nums[i]));            
-            currMin = Math.min(nums[i], Math.min(currMin * nums[i], currMax * nums[i]));
-            
-            currMax = tmpMax;
-            result = Math.max(result, currMax);
-
+            int temp = maxSoFar * nums[i];
+            maxSoFar = Math.max(nums[i], Math.max(nums[i] * maxSoFar, nums[i] * minSoFar));
+            minSoFar = Math.min(temp, Math.min(nums[i] * minSoFar, nums[i]));
+            overAllMax = Math.max(overAllMax, maxSoFar);
         }
-        
-        return result;
+                
+        return overAllMax;
     }
 }
