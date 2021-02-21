588b088463493a40572c66665387cfe534fc8922
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-21 16:51:38 -0600

    Time: 0 ms (100.00%), Space: 38.6 MB (40.56%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,30 +1,33 @@
 class Solution {
     public int findMin(int[] nums) {
-        int low = 0, high = nums.length - 1;
+        int N = nums.length; 
         
-        // already sorted?
-        if (nums[0] <= nums[high]) {
+        if (N == 1) {
             return nums[0];
         }
         
+        if (nums[0] < nums[N - 1]) {
+            return nums[0];
+        }
+        
+        int low = 0;
+        int high = N - 1;
+        
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
             
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            // mid + 1 is smallest
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
+            if (nums[pivot] > nums[pivot + 1]) {
+                return nums[pivot + 1];
             }
             
-            // mid element has to be the smallest 
-            if (nums[mid] < nums[mid - 1]) {
-                return nums[mid];
+            else if (nums[pivot] < nums[pivot - 1]) {
+                return nums[pivot];
             }
             
-            if (nums[low] > nums[mid]) {
-                high = mid - 1;
+            else if (nums[low] > nums[pivot]) {
+                high = pivot - 1;
             } else {
-                low = mid + 1;
+                low = pivot + 1;
             }
         }
         
