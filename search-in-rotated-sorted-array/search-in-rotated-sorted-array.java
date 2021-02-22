339f127f41427d31000b2d8c016930c48cb174c3
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-22 17:05:31 -0600

    Time: 0 ms (100.00%), Space: 39.1 MB (17.59%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -4,26 +4,31 @@ class Solution {
         int high = nums.length - 1;
         
         while (low <= high) {
-            int pivot = low + (high - low) / 2;
-            if (nums[pivot] == target) {
-                return pivot;
+            int mid = low + (high - low) / 2;
+            // LHS of our array in Increasing 
+            // check if our target -> low <= target < high
+            if (nums[mid] == target) {
+                return mid;
             }
             
-            if (nums[pivot] >= nums[low]) {
-                if (target >= nums[low] && target < nums[pivot]) {
-                    high = pivot - 1;
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
                 } else {
-                    low = pivot + 1;
-                }
-            } else {
-                if (target <= nums[high] && target > nums[pivot]) {
-                    low = pivot + 1;
-                } else {
-                    high = pivot - 1;
+                    low = mid + 1;
                 }
             }
-            
+            // RHS of our array in Increasing 
+            // check if our target -> mid < target <= high
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
+        
         return -1;
     }
 }
