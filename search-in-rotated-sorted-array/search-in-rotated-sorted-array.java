99696fd3ccf7df70c2eaa55f5f8f636031995eb4
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-22 18:41:35 -0600

    Time: 0 ms (100.00%), Space: 39.1 MB (15.75%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,25 +1,43 @@
 class Solution {
     public int search(int[] nums, int target) {
+        /*
+            - check if value is mid. return
+            
+            - check if value is in low -> mid
+                LHS is increasing 
+                - if value is nums[low] <= target < nums[mid]
+                        high = mid - 1
+                  else 
+                        low = mid + 1
+                
+            - check if value is in mid -> high
+                RHS is increasing 
+                if value is nums[mid] <= target < nums[high]
+                      low = mid + 1
+                  else 
+                      high = mid - 1
+                        
+            - Return -1 (we never found the target)
+        */
         int low = 0;
         int high = nums.length - 1;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            // LHS of our array in Increasing 
-            // check if our target -> low <= target < high
             if (nums[mid] == target) {
                 return mid;
             }
-            
+            // LHS is increasing
             if (nums[mid] >= nums[low]) {
+                // check if target is inside range 
                 if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             }
-            // RHS of our array in Increasing 
-            // check if our target -> mid < target <= high
+            
+            // RHS is increasing
             else {
                 if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1;
