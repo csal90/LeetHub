a20d13daf14e0fa780add8ac5e643f60f1494dd3
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-22 18:59:14 -0600

    Time: 0 ms (100.00%), Space: 36.4 MB (62.78%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,16 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
         /*
-            nums = [1,2,3,1]
-            nums = [2,7,9,3,1]
+            DP approach: 
+            
+            1) Determine if its a dp problem
+            2) Solve the recurrance relation 
+                - optimized recursion 
+            3) Memoize the pre. sub problems 
+            
+            dp [                         X] N - 1
+                        
+            Base case:
+            dp[0] => nums[0]
+            dp[1] => Max {nums[0], nums[1]}
+            
+            for i <- 2 to n 
+                dp[i] = Max { nums[i] + dp[i - 2], dp[i - 1]}
 
-            dp[nums + 1]
-            dp[0] = nums[0]
-            dp[1] = math.max(dp[0], dp[1])
-            
-           1, 2, 4, 4
-            
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+            return dp[n-1]
+        
         */
         if (nums.length == 0) {
             return 0;
