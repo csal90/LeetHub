93171ced065c7c2ccbb23eb5f4e4d28c8e5d323c
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-22 19:15:44 -0600

    Time: 2 ms (35.40%), Space: 38.8 MB (52.19%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,33 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        /*
+            We need to keep track of the following:
+            - Zeros
+            - Negatives
+            - Positives 
+            
+            maxSoFar = nums[0]
+            minSoFar = nums[0]
+            maxOverall = maxSoFar
+            
+            for i <- 1 to n
+                maxSoFar = Max { nums[i] * maxSoFar, nums[i] * minSoFar, nums[i] }
+                minSoFar = Min { nums[i] * maxSoFar, nums[i] * minSoFar, nums[i] }
+                maxOverall = Max { maxSoFar, maxOverall }
+            
+            return maxOverall
+        */
         int maxSoFar = nums[0];
         int minSoFar = nums[0];
-        int overAllMax = maxSoFar; 
+        int maxOverall = maxSoFar;
         
         for (int i = 1; i < nums.length; i++) {
-            int temp = nums[i] * maxSoFar;
+            int tempMax = nums[i] * maxSoFar;
             maxSoFar = Math.max(nums[i], Math.max(nums[i] * maxSoFar, nums[i] * minSoFar));
-            minSoFar = Math.min(temp, Math.min(nums[i] * minSoFar, nums[i]));
-            overAllMax = Math.max(overAllMax, maxSoFar);
+            minSoFar = Math.min(tempMax, Math.min(nums[i] * minSoFar, nums[i]));
+            maxOverall = Math.max(maxOverall, maxSoFar);
         }
         
-        return overAllMax;
+        return maxOverall;
     }
 }
