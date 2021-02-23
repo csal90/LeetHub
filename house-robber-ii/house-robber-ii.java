9361b6b41695410c41e0c5614be6ee4668565d10
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-22 19:05:53 -0600

    Time: 0 ms (100.00%), Space: 36.8 MB (21.10%) - LeetHub

--- house-robber-ii/house-robber-ii.java
+++ house-robber-ii/house-robber-ii.java
@@ -1,25 +1,38 @@
 class Solution {
-    public static int rob(int[] nums) {
-        int N = nums.length;
-        if (N == 1) {
-            return nums[0];
-        }
-        if (N == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        int [] p1 = Arrays.copyOfRange(nums, 1, N);
-        int [] p2 = Arrays.copyOfRange(nums, 0, N - 1);
-        return Math.max(robHelper(p1), robHelper(p2));
-    }
-    
-    public static int robHelper(int[] nums) {
-        int N = nums.length;
-        int[] dp = new int[N];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < N; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-        return dp[N - 1];
-    }
+    public int rob(int[] nums) {
+         if (nums.length == 0) {
+            return 0;
+        }
+        
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        int[] a1 = Arrays.copyOfRange(nums, 0, nums.length - 1);
+        int[] a2 = Arrays.copyOfRange(nums, 1, nums.length);
+        
+        int r1 = robHelper(a1);
+        int r2 = robHelper(a2);
+        
+        return Math.max(r1, r2);
+    }
+    
+    public int robHelper(int[] nums) { 
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        
+        return dp[nums.length - 1];
+    }
 }
