8a7f4f690ee530ab9b8ba8428bfba54f6a013c16
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-23 17:14:09 -0600

    Time: 0 ms (100.00%), Space: 36 MB (41.99%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -2,16 +2,17 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         
+        // fill dp table with (1)'s
         for (int[] row : dp) {
             Arrays.fill(row, 1);
         }
         
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i][j-1] + dp[i - 1][j];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
