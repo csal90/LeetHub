146611c62bad0eaf09eba7d50107c35488d8b21a
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-24 09:04:16 -0600

    Time: 68 ms (25.89%), Space: 38.6 MB (72.73%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,25 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // Base cases 
         if (nums.length == 1) {
             return 1;
         }
         
-        // Fill the dp array with (1) at each position we have at list a sequence of 1
         int max = 0;
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
+        Arrays.fill(dp, 1); // every number is considered it's own subsequence 
         
-        // Iterate over the array (start from 1)
         for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) { // iterate all the elements before i
-                if (nums[j] < nums[i]) { // only then it's an increasing subsequence 
-                    dp[i] = Math.max(dp[i], dp[j] + 1); // it's possible we have computed a longer LIS, this is why we take the max of the previous solution, OR dp[j] + 1 indicating we should include thise element 
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) { // theres a number we can use in or LIS
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-                max = Math.max(max, dp[i]);
+                max = Math.max(dp[i], max);
             }
         }
         
         return max;
+        
     }
 }
