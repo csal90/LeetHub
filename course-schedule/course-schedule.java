21754a0a969e354ba367a8e93ab1af051b108f13
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-25 12:01:09 -0600

    Time: 19 ms (23.36%), Space: 39.5 MB (78.74%) - LeetHub

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        /*
+             ** TOPOLOGICAL SORTING ** 
+            Keep track of nodes indegree
+            if at the end all nodes indegree equal 0, then we have a DAG,
+            which means the course schedule can be finished.    
+        */
         int[] indegree = new int[numCourses];
         
+        // Indegree - how many prerequisites are needed
         for (int i = 0; i < prerequisites.length; i++) {
-            indegree[prerequisites[i][0]]++; // Indegree - how many prerequisites are needed
+            indegree[prerequisites[i][0]]++; 
         }
-        
+                
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
@@ -14,20 +21,22 @@ class Solution {
         }
         
         // How many course don't need prerequisites
-        int canFinish = queue.size();
+        int count = queue.size();
+        
+        // BFS
         while (!queue.isEmpty()) {
-            int prerequisite = queue.remove();
+            int curr = queue.poll();
             for (int i = 0; i < prerequisites.length; i++) {
-                if (prerequisites[i][1] == prerequisite) {
+                if (prerequisites[i][1] == curr) {
                     indegree[prerequisites[i][0]]--;
                     if (indegree[prerequisites[i][0]] == 0) {
-                        canFinish++;
+                        count++;
                         queue.add(prerequisites[i][0]);
                     }
                 }
             }
         }
         
-        return (canFinish == numCourses);
+        return (count == numCourses);
     }
 }
