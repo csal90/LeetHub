84ce917efcd1871b7787ee82e88cf43d6f7cc4dc
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-02-28 16:16:52 -0600

    Time: 24 ms (98.98%), Space: 39.3 MB (41.36%) - LeetHub

--- clone-graph/clone-graph.java
+++ clone-graph/clone-graph.java
@@ -21,28 +21,17 @@ class Node {
 class Solution {
     HashMap<Node, Node> visited = new HashMap<>();
     public Node cloneGraph(Node node) {
-        /*
-            Clone as you go
-            Global hashtable k,v pair <Node, Node> 
-                                        OG , CLONE
-            recursion to recursively keep created cloned nodes
-            and iterating through them and adding them to the nodes adj. list         
-        */
         if (node == null) {
-            return node; 
+            return node;
         }
-        
         if (visited.containsKey(node)) {
             return visited.get(node);
         }
-        
-        // Main DFS 
         Node clone = new Node(node.val, new ArrayList());
         visited.put(node, clone);
         for (Node neighbor : node.neighbors) {
             clone.neighbors.add(cloneGraph(neighbor));
         }
-        
         return clone;
     }
 }
