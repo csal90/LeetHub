84b0a49d93bf2659c38332675605fa74bc98fd0f
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-03-03 16:35:18 -0600

    Time: 1 ms (100.00%), Space: 39.3 MB (30.61%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.java
+++ non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,34 +1,22 @@
 class Solution {
-    /*
-        GREEDILY CHOOSE BASED ON END POINTS
-        - Sort the intervals based on end points 
-        - Keep track of end time
-        - If current start time is less than the previous
-          end time, we have an interval. Count!
-        
-        [[1,2],[1,3],[2,3],[3,4]]
-        
-        
-    */
     public int eraseOverlapIntervals(int[][] intervals) {
-        if(intervals == null || intervals.length == 0) 
-            return 0;
+        if (intervals.length == 0) return 0;
         
-        Arrays.sort(intervals,(a,b) -> a[1] - b[1]);
+        int count = 0;
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]); // sort by start time 
         
-        int remove = 0;
-        int prevEnd = intervals[0][1]; 
-        
-        for(int i = 1; i < intervals.length; i++) {
-            int currentStart = intervals[i][0];
-            if (currentStart < prevEnd) { // if current start time is less than previous end time, overlap!
-                remove++;
-            } else { // if it does not overlap we simply update our end time to current end time
-                prevEnd = intervals[i][1]; // no overlap, update new end time
+        int[] prev = intervals[0]; // first interval 
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            if (curr[0] < prev[1]) { // overlap
+                count += 1;
+                prev[1] = Math.min(prev[1], curr[1]);
+            } else {
+                prev = curr;
             }
         }
         
-        return remove;
-        
+        return count;
     }
 }
