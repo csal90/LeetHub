3a676e4491b1f6573344834711f274fa2a02beb1
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-03-07 17:23:42 -0600

    Time: 0 ms (100.00%), Space: 38.6 MB (39.37%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,5 @@
 class Solution {
     public int search(int[] nums, int target) {
-        /*
-            - check if value is mid. return
-            
-            - check if value is in low -> mid
-                LHS is increasing 
-                - if value is nums[low] <= target < nums[mid]
-                        high = mid - 1
-                  else 
-                        low = mid + 1
-                
-            - check if value is in mid -> high
-                RHS is increasing 
-                if value is nums[mid] <= target < nums[high]
-                      low = mid + 1
-                  else 
-                      high = mid - 1
-                        
-            - Return -1 (we never found the target)
-        */
         int low = 0;
         int high = nums.length - 1;
         
@@ -27,22 +8,18 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             }
-            // LHS is increasing
-            if (nums[mid] >= nums[low]) {
-                // check if target is inside range 
-                if (target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            
-            // RHS is increasing
-            else {
+            if (nums[mid] < nums[high]) {
                 if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
+                }
+            } 
+            else { // CHECK IF TARGET IS IN LHS LOW >= TARGET <= HIGH
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
             }
         }
