912a1f0ce45bb5b246e7529e4376d6d29565ba5d
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-03-07 17:03:58 -0600

    Time: 0 ms (100.00%), Space: 38.2 MB (89.12%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,53 +1,16 @@
 class Solution {
-    public int findMin(int[] nums) {
-                /*
-            - we need to check if array is already sorted
-            if (nums[0] < nums[N-1]) return nums[0]
-            
-            - we need to check if the array length is 1
-            if (N == 1) return nums[0]
-            
-            - Modified binary search:
-            
-            1) checking nums[mid] < nums[mid + 1] 
-                return nums[mid + 1]
-            
-            2) checking nums[mid - 1] < nums[mid]
-                return nums[mid]
-                
-            Updating our pointers. 
-            
-            if (nums[mid] < nums[0]) 
-                high = mid - 1
-            else
-                low = mid + 1            
-        */
-        
-        int low = 0;
-        int high = nums.length - 1;
-        
-        if (nums[0] < nums[nums.length - 1]) {
-            return nums[0];
-        }
-        
-        if (nums.length == 1) {
-            return nums[0];
-        }
+    public int findMin(int[] nums) {        
+        int low = 0, high = nums.length - 1;
         
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
-            }
-            if (nums[mid] > nums[0]) {
-                low = mid + 1;
+            if (nums[mid] < nums[high]) {
+                high = mid;
             } else {
-                high = mid - 1;
+                low = mid + 1;
             }
         }
-        return -1;
+        
+        return nums[low];
     }
 }
