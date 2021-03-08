316b56b9a74c86295e8b45207e0eeeb469ab52f1
Christian Salas <35342933+csal90@users.noreply.github.com>
2021-03-07 18:08:15 -0600

    Time: 6 ms (64.86%), Space: 39.3 MB (61.37%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        
-        int N = s.length();
+        boolean[] dp = new boolean[s.length() + 1];
         Set<String> set = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[N + 1];
-        
         dp[0] = true;
-        for (int i = 1; i <= N; i++) {
+        
+        for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
                 if (dp[j] && set.contains(s.substring(j, i))) {
                     dp[i] = true;
@@ -14,6 +12,7 @@ class Solution {
                 }
             }
         }
-        return dp[N];
+        
+        return dp[s.length()];
     }
 }
